8698a5178a93cbc954929432909b95b5fd5c1893
Shaurya <emorres25@gmail.com>
2016-08-16 15:57:40 +0530

    argv support added, plus code refinement

--- im_scrapin.py
+++ im_scrapin.py
@@ -2,6 +2,7 @@
 
 from bs4 import BeautifulSoup
 import urllib2
+import sys
 
 url = "http://www.imdb.com/search/title?title="
 
@@ -18,48 +19,50 @@ def qurl(query):
 			#print url
 	return url
 
-query = raw_input("Enter the movie name: ").split(" ")
-#print query
-final_url = qurl(query)
-#print final_url
+def bsmagic(final_url):
+	content = urllib2.urlopen(final_url)
+	soup = BeautifulSoup(content, 'lxml')
+	
+	
+	#rating extraction
+	ratings_arr= []
+	for stuff in soup.findAll('div', {'class','ratings-imdb-rating'}):
+		ratings_arr.append(stuff['data-value'])
 
-content = urllib2.urlopen(final_url)
-soup = BeautifulSoup(content, 'lxml')
+	ratings_arr = ratings_arr[:5]
+	#print ratings_arr
+	
+	#title extraction
+	titles_arr = []
+	for titles in soup.findAll('h3', {'class', 'lister-item-header'}):
+		titles_arr.append(titles.a.string)
 
+	titles_arr = titles_arr[:5]
+	#print titles_arr
+	
+	#years extraction
+	years_arr = []
+	for years in soup.findAll('span', {'class', 'lister-item-year'}):
+		years_arr.append(years.string)
+	years_arr = years_arr[:5]
+	#print years_arr
+	output(titles_arr, ratings_arr, years_arr)
 
-#rating extraction
-ratings_arr= []
-for stuff in soup.findAll('div', {'class','ratings-imdb-rating'}):
-	ratings_arr.append(stuff['data-value'])
+def output(titles_arr, ratings_arr, years_arr):
+	i=0
+	for titles in titles_arr:
+		try:
+			print ' > ' + titles + years_arr[i] + '  - ' + ratings_arr[i]
+			i += 1
+		except TypeError:
+			print ' > ' + str(titles) + '(' +str(years_arr[i]) + ')' + '  - ' + str(ratings_arr[i])
 
-ratings_arr = ratings_arr[:5]
-#print ratings_arr
+if __name__ == '__main__':
+	query = sys.argv[1:]
+	#print query
 
-#title extraction
-titles_arr = []
-for titles in soup.findAll('h3', {'class', 'lister-item-header'}):
-	titles_arr.append(titles.a.string)
+	final_url = qurl(query)
+	#print final_url	
 
-titles_arr = titles_arr[:5]
-#print titles_arr
-
-
-
-#years extraction
-years_arr = []
-for years in soup.findAll('span', {'class', 'lister-item-year'}):
-	years_arr.append(years.string)
-years_arr = years_arr[:5]
-#print years_arr
-
-#printing out stuff
-
-i=0
-for titles in titles_arr:
-	try:
-		print ' > ' + titles + years_arr[i] + '  - ' + ratings_arr[i]
-		i += 1
-	except TypeError:
-		print ' > ' + str(titles) + '(' +str(years_arr[i]) + ')' + '  - ' + str(ratings_arr[i])
-
-raw_input("")
+	bsmagic(final_url)
+	raw_input("")
