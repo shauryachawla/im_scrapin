ffea8059d0dc85b039582603728f4d51f37fb155
Shaurya <emorres25@gmail.com>
2016-08-16 10:32:06 +0530

    Added error exceptions

--- im_scrapin.py
+++ im_scrapin.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 from bs4 import BeautifulSoup
 import urllib2
 
@@ -31,7 +33,7 @@ for stuff in soup.findAll('div', {'class','ratings-imdb-rating'}):
 	ratings_arr.append(stuff['data-value'])
 
 ratings_arr = ratings_arr[:5]
-
+#print ratings_arr
 
 #title extraction
 titles_arr = []
@@ -39,7 +41,7 @@ for titles in soup.findAll('h3', {'class', 'lister-item-header'}):
 	titles_arr.append(titles.a.string)
 
 titles_arr = titles_arr[:5]
-#print titles_arr
+p#rint titles_arr
 
 
 
@@ -48,10 +50,16 @@ years_arr = []
 for years in soup.findAll('span', {'class', 'lister-item-year'}):
 	years_arr.append(years.string)
 years_arr = years_arr[:5]
-
+#print years_arr
 
 #printing out stuff
+
 i=0
 for titles in titles_arr:
-	print ' > ' + titles + years_arr[i] + '  - ' + ratings_arr[i]
-	i += 1
+	try:
+		print ' > ' + titles + years_arr[i] + '  - ' + ratings_arr[i]
+		i += 1
+	except TypeError:
+		print ' > ' + str(titles) + '(' +str(years_arr[i]) + ')' + '  - ' + str(ratings_arr[i])
+
+raw_input("")
